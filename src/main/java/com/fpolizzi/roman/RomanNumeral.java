46360475019940e3a3322202ad9c8856b58ec243
Franco Polizzi <me@fpolizzi.com>
2019-12-23 21:55:53 +0100

    Add class RomanNumeralTest as testing class for RomanNumeral

--- src/main/java/com/fpolizzi/roman/RomanNumeral.java
+++ src/main/java/com/fpolizzi/roman/RomanNumeral.java
@@ -1,7 +1,16 @@
 package com.fpolizzi.roman;
 
 public class RomanNumeral {
+    int i = 1;
+    int v = 5;
+    int x = 10;
+    int l = 50;
+    int c = 100;
+    int d = 500;
+    int m = 1000;
+
     public int convert(String romanNumeral) {
+        System.out.println(romanNumeral);
         return 0;
     }
 }
---
+++ src/main/java/com/fpolizzi/roman/RomanNumeralTest.java
@@ -0,0 +1,11 @@
+package com.fpolizzi.roman;
+
+public class RomanNumeralTest {
+    RomanNumeral roman = new RomanNumeral();
+
+    public static void main(String[] args) {
+        RomanNumeral roman = new RomanNumeral();
+
+        int test = roman.convert("IV");
+    }
+}
